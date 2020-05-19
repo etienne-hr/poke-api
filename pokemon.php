bf8823085b0b9b4eddba0645c3b1586c4bca49cd
etienne-hr <etienne.haller@hetic.net>
2020-05-19 14:06:31 +0200

    pages details for pokemon :sparkles:

--- pokemon.php
+++ pokemon.php
@@ -1,9 +1,33 @@
 <?php
     include './config.php';
 
-    // echo '<script type="text/javascript">
-    //         let pokemonWeight = []
-    //       </script>';
+    $pokemon = $_GET['pokemon'];
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'https://pokeapi.co/api/v2/pokemon/'.$pokemon.'');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    $data = json_decode($result);
+
+    
+    $name = $data->name;
+
+    $height = $data->height;
+
+    $weight = $data->weight;
+
+    $stats = $data->stats[5]->base_stat;
+
+    $pokemonImage = $data->sprites->front_default;
+
+    $typeZero = $data->types[0]->type->name;
+
+    if(isset($data->types[1]))
+        {   
+            $typeOne = $data->types[1]->type->name;
+        }
+
 
 ?>
 <!DOCTYPE html>
